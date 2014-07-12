53dd51547a2fe6bb54559ef64ccdbb04c15883d9
George Song <george@55minutes.com>
2014-07-12 08:24:56 -0700

    Wrap in IIFE for strict mode

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,12 @@
+(function(window, angular, undefined) {
+
+"use strict";
+
 angular.module('plan4me', [])
 .controller('AddListItemController', ['$scope', function($scope) {
 	$scope.addTask = function() {
 		$scope.newTask = $scope.inputTask;
 	};
 }]);
+
+})(window, window.angular);
