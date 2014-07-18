f29b2fb5dee4206ee60b23c8781ac23a4c47d2a0
George Song <george@55minutes.com>
2014-07-17 22:31:15 -0700

    Formatting

--- scripts/main.js
+++ scripts/main.js
@@ -3,22 +3,22 @@
 "use strict";
 
 // baseRef - starting point "/"
-angular.module('planhacker', ['firebase'])
+angular.module("planhacker", ["firebase"])
 
 .factory("baseRef", ["$firebase", function($firebase) {
     return $firebase(new Firebase("https://blazing-fire-2043.firebaseio.com"));
 }])
 
 .factory("users", ["baseRef", function(baseRef) {
-    return baseRef.$child('users');
+    return baseRef.$child("users");
 }])
 
 .factory("user", ["users", function(users) {
-    return users.$child('user');
+    return users.$child("user");
 }])
 
 .factory("tasks", ["user", function(user) {
-    return user.$child('tasks');
+    return user.$child("tasks");
 }])
 
 .controller("TaskListController", ["$scope", "tasks", function($scope, tasks) {
