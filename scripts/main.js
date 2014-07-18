8cdeffff3188b0b5ac481782e5cdca45e9c31054
George Song <george@55minutes.com>
2014-07-18 06:38:18 -0700

    JS formatting

--- scripts/main.js
+++ scripts/main.js
@@ -5,19 +5,19 @@
 angular.module("planhacker", ["firebase"])
 
 .factory("baseRef", ["$firebase", function($firebase) {
-    return $firebase(new Firebase("https://planhacker.firebaseio.com"));
+  return $firebase(new Firebase("https://planhacker.firebaseio.com"));
 }])
 
 .factory("users", ["baseRef", function(baseRef) {
-    return baseRef.$child("users");
+  return baseRef.$child("users");
 }])
 
 .factory("user", ["users", function(users) {
-    return users.$child("user");
+  return users.$child("user");
 }])
 
 .factory("tasks", ["user", function(user) {
-    return user.$child("tasks");
+  return user.$child("tasks");
 }])
 
 .controller("TaskListController", ["$scope", "tasks", function($scope, tasks) {
