42ad3414ce2b88238b32777375e2bd227b27f682
Hackbright Student <info@hackbrightacademy.com>
2014-07-21 23:19:40 -0700

    Add a label and assign a label to a task... work in progress...

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 
 "use strict";
 
-angular.module("planhacker", ["firebase"])
+angular.module("planhacker", ["firebase", "ui.bootstrap"])
 
 .factory("baseRef", ["$firebase", function($firebase) {
   return $firebase(new Firebase("https://planhacker.firebaseio.com"));
@@ -24,31 +24,61 @@ angular.module("planhacker", ["firebase"])
   return user.$child("completedTasks");
 }])
 
+.factory("labels", ["user", function(user) {
+  return user.$child("labels");
+}])
+
+// I don't think I need this factory... 
+// .factory("label", ["tasks", function(tasks) {
+//   return tasks.$child("label");
+// }])
+
 .controller("TaskListController", [
-  "$scope", "$filter", "tasks", "completedTasks",
-  function($scope, $filter, tasks, completedTasks) {
+  "$scope", "$filter", "tasks", "completedTasks", "labels",
+  function($scope, $filter, tasks, completedTasks, labels) {
 
   tasks.$bind($scope, "tasks");
   completedTasks.$bind($scope, "completedTasks");
 
   $scope.addTask = function() {
-    var promise = tasks.$add({name: $scope.taskInput, isComplete: false});
-    promise.then(function(ref) {
+    tasks.$add({name: $scope.taskInput, isComplete: false}).then(function(ref) {
       ref.setPriority(-new Date().getTime());
     });
-
     $scope.taskInput = null;
   };
 
   $scope.completeTask = function(task) {
     completedTasks.$add({name: task.name, isComplete: true});
     tasks.$remove(task.$id);
-  }
+  };
 
   $scope.incompleteTask = function(task) {
     tasks.$add({name: task.name, isComplete: false});
     completedTasks.$remove(task.$id);
-  }
+  };
+
+  $scope.showLabel = false;
+
+}])
+
+.controller("LabelController", ["$scope", "$filter", "labels", "tasks",
+  function($scope, $filter, labels, tasks) {
+
+  labels.$bind($scope, "labels");
+
+  $scope.addLabel = function(task) {
+    labels.$add({label: $scope.labelInput});
+  };
+
+  $scope.assignLabel = function(item) {
+    tasks.$update({label: item.label});
+    // Struggling with this right now. I am trying to add an attribute to a task in "tasks" URL
+    // I tried $add and $update. They don't work the way I want it...
+  };
+
+  $scope.printHello = function() {
+    console.log("Hello!");
+  };
 
 }]);
 
